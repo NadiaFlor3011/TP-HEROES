f537b55eeec5ec3c5f97cffa6c6a5c9db97d6c98
Nadia Florencia Maldonado <nadiamaldonado3011@gmail.com>
2022-06-22 12:43:14 -0300

    se agregaron imagenes

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const path = require('path');
 const app = express();
 const port = 3030;
 
+/*recursos estáticos*/
+app.use(express.static('public'));
+
+
+/*rutas*/
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'index.html')));
 
 app.get('/babbage', (req, res) => res.sendFile(path.join(__dirname, 'views', 'babbage.html')));
@@ -19,5 +24,5 @@ app.get('/lovelace', (req, res) => res.sendFile(path.join(__dirname, 'views', 'l
 
 app.get('/turing', (req, res) => res.sendFile(path.join(__dirname, 'views', 'turing.html')));
 
-
+/*servidor*/
 app.listen(port, () => console.log('http://localhost:' + port));
